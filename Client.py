99d6695bfae176e787fd17566d630a8a68026483
tongplw <bangkok.tong@gmail.com>
2020-02-26 15:17:30 +0700

    first commit

---
+++ Client.py
@@ -0,0 +1,43 @@
+import sys
+import socket
+import threading
+
+
+class Client():
+
+    def __init__(self, client_name):
+
+        # Create a TCP/IP socket and connect the socket to the port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = ('localhost', 8080)
+        self.socket.connect(self.server_address)
+        self.socket.setblocking(1)
+
+        self.client_name = client_name
+        send = threading.Thread(target=self._client_send)
+        send.start()
+        receive = threading.Thread(target=self._client_receive)
+        receive.start()
+
+    def _client_send(self):
+        self.socket.send(bytes(self.client_name, encoding='utf-8'))  
+        while True:
+            try:
+                c = input(f'{self.client_name}: ')
+                self.socket.send(bytes(c, encoding='utf-8'))  
+            except:
+                self.socket.close()
+                return
+
+    def _client_receive(self):
+        while True:
+            try:
+                print(self.socket.recv(1024).decode("utf-8"))
+            except:
+                self.socket.close()
+                return
+
+
+if __name__ == "__main__":
+    client_name = sys.argv[1]
+    Client(client_name)
---
+++ Server.py
@@ -0,0 +1,53 @@
+import socket
+import _thread
+import threading
+
+
+class Server():
+
+    def __init__(self):
+
+        # For remembering users
+        self.users_table = {}
+
+        # Create a TCP/IP socket and bind it the Socket to the port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = ('localhost', 8080)
+        self.socket.bind(self.server_address)
+        self.socket.setblocking(1)
+        self.socket.listen(10)
+        print('Starting up on {} port {}'.format(*self.server_address))
+        self._wait_for_new_connections()
+
+    def _wait_for_new_connections(self):
+        while True:
+            connection, _ = self.socket.accept()
+            _thread.start_new_thread(self._on_new_client, (connection,))
+
+    def _on_new_client(self, connection):
+        try:
+            # Declare the client's name
+            client_name = connection.recv(64).decode('utf-8')
+            self.users_table[connection] = client_name
+            print(f'{client_name} joined the room !!')
+
+            while True:
+                data = connection.recv(64).decode('utf-8')
+                if data != '':
+                    self.multicast(data, owner=connection)
+                else:
+                    return 
+        except:
+            print(f'{client_name} left the room !!')
+            self.users_table.pop(connection)
+            connection.close()
+
+    def multicast(self, message, owner=None):
+        for conn in self.users_table:
+            if conn == owner: continue
+            data = f'{self.users_table[owner]}: {message}'
+            conn.sendall(bytes(data, encoding='utf-8'))  
+
+
+if __name__ == "__main__":
+    Server()
